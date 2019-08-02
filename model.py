facbdd65366171c3bab5388f82e55ca5e209dbd4
Prateek Kumar Agnihotri <44502830+PRATEEKKUMARAGNIHOTRI@users.noreply.github.com>
2019-08-02 19:12:56 +0530

    Update model.py

--- model.py
+++ model.py
@@ -23,7 +23,7 @@ class Model(object):
 	self.input_shape = [128,128,3]
 	self.nb_filters = 3
 
-    def encoder(self, images, reuse=False, return_feat=False):
+"""    def encoder(self, images, reuse=False, return_feat=False):
 	
 	conv_args = dict(
         activation=tf.nn.leaky_relu,
@@ -47,7 +47,26 @@ class Model(object):
 		    if return_feat:
 			return net	
 		    net = slim.fully_connected(net, self.no_classes, activation_fn=None, scope='fco')
+		    return net"""
+
+    def encoder(self, images, reuse=False, return_feat=False):
+
+	with tf.variable_scope('encoder', reuse=reuse):
+	    with slim.arg_scope([slim.fully_connected], activation_fn=tf.nn.relu):
+		with slim.arg_scope([slim.conv2d], activation_fn=tf.nn.relu, padding='VALID'):
+
+		    net = slim.conv2d(images, 64, 5, scope='conv1')
+		    net = slim.max_pool2d(net, 2, stride=2, scope='pool1')
+		    net = slim.conv2d(net, 128, 5, scope='conv2')
+		    net = slim.max_pool2d(net, 2, stride=2, scope='pool2')
+		    net = tf.contrib.layers.flatten(net)
+		    net = slim.fully_connected(net, 1024, scope='fc1')
+		    net = slim.fully_connected(net, 1024, scope='fc2')
+		    if return_feat:
+			return net	
+		    net = slim.fully_connected(net, self.no_classes, activation_fn=None, scope='fco')
 		    return net
+
 		
 	
 	
