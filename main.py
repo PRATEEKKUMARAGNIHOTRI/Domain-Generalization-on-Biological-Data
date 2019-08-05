f601f2802fa09b41255a4053d3d46409437b914b
Prateek Kumar Agnihotri <44502830+PRATEEKKUMARAGNIHOTRI@users.noreply.github.com>
2019-08-05 12:26:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import gc
+gc.collect()
+
 import tensorflow as tf
 from model import Model
 from trainOps import TrainOps
@@ -8,28 +11,11 @@ import cPickle
 import numpy.random as npr
 import numpy as np
 
-def main(_):
+EXP_DIR = "./"
 
-    EXP_DIR = "./"
+model = Model()
+trainOps = TrainOps(model, EXP_DIR)
+trainOps.load_exp_config()
 
-    model = Model()
-    trainOps = TrainOps(model, EXP_DIR)
-    trainOps.load_exp_config()
-    
-    print("Training")
-    trainOps.train()       
-    print 'Testing'
-    trainOps.test('svhn')       
-
-if __name__ == '__main__':
-    tf.app.run()
-
-
-
-    
-
-
-
-
-
-
+print("Training")
+trainOps.train()
